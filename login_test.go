56a761b1be9a0413e5f87cea98317e460f6e12ae
Aslak Knutsen <aslak@4fs.no>
2016-09-19 21:26:08 -0700

    Remove Login::Authorize OK response

--- login_test.go
+++ login_test.go
@@ -3,16 +3,19 @@ package main
 import (
 	"testing"
 
+	"github.com/almighty/almighty-core/app"
 	"github.com/almighty/almighty-core/app/test"
 	"github.com/almighty/almighty-core/resource"
 )
 
 func TestAuthorizeLoginOK(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
-	controller := LoginController{}
-	_, res := test.AuthorizeLoginOK(t, nil, nil, &controller)
+	controller := LoginController{auth: TestLoginService{}}
+	test.AuthorizeLoginTemporaryRedirect(t, nil, nil, &controller)
+}
 
-	if res.Token == "" {
-		t.Error("Token not generated")
-	}
+type TestLoginService struct{}
+
+func (t TestLoginService) Perform(ctx *app.AuthorizeLoginContext) error {
+	return ctx.TemporaryRedirect()
 }
