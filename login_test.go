18552d11e93a60539c2adeada2e989360957ac60
Pranav Gore <pranavgore09@gmail.com>
2016-10-12 14:24:05 +0530

    merge-removing version_test.go

--- login_test.go
+++ login_test.go
@@ -3,19 +3,16 @@ package main
 import (
 	"testing"
 
-	"github.com/almighty/almighty-core/app"
 	"github.com/almighty/almighty-core/app/test"
 	"github.com/almighty/almighty-core/resource"
 )
 
 func TestAuthorizeLoginOK(t *testing.T) {
 	resource.Require(t, resource.UnitTest)
-	controller := LoginController{auth: TestLoginService{}}
-	test.AuthorizeLoginTemporaryRedirect(t, nil, nil, &controller)
-}
+	controller := LoginController{}
+	_, res := test.AuthorizeLoginOK(t, nil, nil, &controller)
 
-type TestLoginService struct{}
-
-func (t TestLoginService) Perform(ctx *app.AuthorizeLoginContext) error {
-	return ctx.TemporaryRedirect()
+	if res.Token == "" {
+		t.Error("Token not generated")
+	}
 }
