010233a98cab2cfc057c23a40071a006e3793e12
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-11-11 15:34:27 +0530

    read keys from config
    test empty result

--- identity_blackbox_test.go
+++ identity_blackbox_test.go
@@ -6,19 +6,24 @@ import (
 	. "github.com/almighty/almighty-core"
 	"github.com/almighty/almighty-core/account"
 	"github.com/almighty/almighty-core/app/test"
+	"github.com/almighty/almighty-core/configuration"
 	"github.com/almighty/almighty-core/gormapplication"
 	"github.com/almighty/almighty-core/resource"
 	testsupport "github.com/almighty/almighty-core/test"
 	almtoken "github.com/almighty/almighty-core/token"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
 )
 
 func TestListIdentities(t *testing.T) {
 	resource.Require(t, resource.Database)
-	pub, _ := almtoken.ParsePublicKey([]byte(almtoken.RSAPublicKey))
-	priv, _ := almtoken.ParsePrivateKey([]byte(almtoken.RSAPrivateKey))
+	pub, _ := almtoken.ParsePublicKey(configuration.GetTokenPublicKey())
+	priv, _ := almtoken.ParsePrivateKey(configuration.GetTokenPrivateKey())
 	service := testsupport.ServiceAsUser("TestListIdentities-Service", almtoken.NewManager(pub, priv), account.TestIdentity)
+
+	identityController := NewIdentityController(service, gormapplication.NewGormDB(DB))
+	_, ic := test.ListIdentityOK(t, service.Context, service, identityController)
+	require.NotNil(t, ic)
 
 	ctx := context.Background()
 	identityRepo := account.NewIdentityRepository(DB)
@@ -34,8 +39,7 @@ func TestListIdentities(t *testing.T) {
 	defer func() {
 		DB.Unscoped().Delete(&identity)
 	}()
-	identityController := NewIdentityController(service, gormapplication.NewGormDB(DB))
 
-	_, ic := test.ListIdentityOK(t, service.Context, service, identityController)
-	assert.NotNil(t, ic)
+	_, ic2 := test.ListIdentityOK(t, service.Context, service, identityController)
+	require.NotNil(t, ic)
 }
