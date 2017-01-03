898a37bb50a1ad9f65013d30a5e757ce06c04d32
Pranav Gore <pranavgore09@gmail.com>
2017-01-03 13:35:08 +0530

    List tracker and Show tracker - jsonapi compatile Depends on PR#626 Fixes #468 List and Show Tracker now follow jsonapi specification. Tests updated as needed.

--- tracker.go
+++ tracker.go
@@ -99,7 +99,13 @@ func (c *TrackerController) Show(ctx *app.ShowTrackerContext) error {
 				return ctx.ResponseData.Service.Send(ctx.Context, httpStatusCode, jerrors)
 			}
 		}
-		return ctx.OK(t)
+		jsonapiTrackerObject := app.TrackerObjectSingle{
+			Data: convertTracker(t),
+			Links: &app.TrackerLinks{
+				Self: buildAbsoluteURL(ctx.RequestData),
+			},
+		}
+		return ctx.OK(&jsonapiTrackerObject)
 	})
 }
 
@@ -121,7 +127,17 @@ func (c *TrackerController) List(ctx *app.ListTrackerContext) error {
 			jerrors, _ := jsonapi.ErrorToJSONAPIErrors(goa.ErrInternal(fmt.Sprintf("Error listing trackers: %s", err.Error())))
 			return ctx.InternalServerError(jerrors)
 		}
-		return ctx.OK(result)
+		jsonapiData := make([]*app.TrackerData, len(result))
+		for i, tracker := range result {
+			jsonapiData[i] = convertTracker(tracker)
+		}
+		response := app.TrackerObjectList{
+			Data: jsonapiData,
+			Links: &app.TrackerLinks{
+				Self: buildAbsoluteURL(ctx.RequestData),
+			},
+		}
+		return ctx.OK(&response)
 	})
 
 }
@@ -151,4 +167,16 @@ func (c *TrackerController) Update(ctx *app.UpdateTrackerContext) error {
 	})
 	c.scheduler.ScheduleAllQueries()
 	return result
+}
+
+// convertTracker converts app.Tracker object into jsonapi based app.TrackerData
+func convertTracker(t *app.Tracker) *app.TrackerData {
+	return &app.TrackerData{
+		ID:   &t.ID,
+		Type: APIStringTypeTracker,
+		Attributes: &app.TrackerAttributes{
+			Type: t.Type,
+			URL:  t.URL,
+		},
+	}
 }
